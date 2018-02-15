8a9af424231b86936214e0bf66832be878546b34
linescoin <metaln33@gmail.com>
2018-02-16 01:43:24 +0300

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var api = 'http://192.168.1.3:15011';
+var blockTargetInterval = 240;
+var coinUnits = 1000000000000;
+var symbol = 'LNS';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "lns": [
+	
+    ]
+};
