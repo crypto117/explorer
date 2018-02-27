c41cb1d2b31bc34c5946bcb1dabb6511f2e219f8
linescoin <metaln33@gmail.com>
2018-02-27 11:40:31 +0000

    config changed

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-var api = 'http://192.168.1.3:15011';
-var blockTargetInterval = 240;
-var coinUnits = 1000000000000;
+var api = 'http://explorer.lines.click:15011';
+var blockTargetInterval = 120.0;
+var coinUnits = 100000000;
 var symbol = 'LNS';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "lns": [
-	
+        ["pool.lines.click", "http://pool.lines.click:8117"],	
     ]
 };
