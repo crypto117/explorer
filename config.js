3b8b0bd7bab6acf25d95916c8eb35c9f747cd28c
linescoin <metaln33@gmail.com>
2018-03-15 14:29:22 +0000

    movead to new domain

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-var api = 'http://explorer.lines.click:15011';
+var api = 'http://explorer.lines.pw:15015';
 var blockTargetInterval = 120.0;
-var coinUnits = 100000000;
+var coinUnits = 10000000;
 var symbol = 'LNS';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "lns": [
-        ["pool.lines.click", "http://pool.lines.click:8117"],	
+        ["pool.lines.pw", "http://pool.lines.pw:8117"],	
     ]
 };
