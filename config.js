150cee443d4d0cc4ea88438c70f908e32584461c
linescoin <metaln33@gmail.com>
2018-11-04 10:41:17 +0000

    explorer api bugfixes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://explorer.lines.pw:15015';
+var api = 'http://explorer.lines.pw/API';
 var blockTargetInterval = 120.0;
 var coinUnits = 10000000;
 var symbol = 'LNS';
@@ -6,6 +6,19 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "lns": [
-        ["pool.lines.pw", "http://pool.lines.pw:8117"],	
+        ["pool.lines.pw", "https://pool.lines.pw/API"],
+        ["cryptoknight.cc/lines", "http://94.130.207.37:40004"],
+        ["lines.fatpanda.club", "http://54.148.101.99:8016"],
+        ["youpool.io/LNS/", "http://118.178.122.224:8110"],
+        ["lines.hpool.net", "http://server1.chinanorth.cloudapp.chinacloudapi.cn:18702"],
+        ["lns.euminingpool.com", "http://lns.euminingpool.com:15016"],
+        ["pool.criptolearn.net/lines/", "http://pool.criptolearn.net:8118"],
+        ["kenta.online", "http://172.104.145.18:8117"],
+        ["miningpool365.com/lines/", "http://miningpool365.com:8118"],
+        ["lnspool.partyvibe.com", "http://lnspool.partyvibe.com:8118"],
+        ["lns-us.crypt29.net", "https://lns-us.crypt29.net/lns-api/"],
+        ["superblockchain.con-ip.com/lines/", "http://superblockchain.con-ip.com:8045"],
+        ["lns.go-mine.it", "http://lns.go-mine.it:8001"],
+        ["lines.clevery.xyz", "https://lines.clevery.xyz/api"]
     ]
 };
--- q/supply/index.php
+++ q/supply/index.php
@@ -2,7 +2,7 @@
 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
 
-$ch = curl_init('http://45.32.232.11:32348/json_rpc');
+$ch = curl_init('http://explorer.lines.pw/API/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -29,7 +29,7 @@ curl_close($ch);
 $data_string2 = '{"jsonrpc":"2.0","id":"test","method":"f_block_json","params":{"hash":"'.$hash.'"}}';
 
 
-$ch2 = curl_init('http://45.32.232.11:32348/json_rpc');
+$ch2 = curl_init('http://explorer.lines.pw/API/json_rpc');
 curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch2, CURLOPT_POSTFIELDS, $data_string2);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
@@ -47,7 +47,7 @@ $blockData = json_decode($block, TRUE);
 
 $supply = $blockData[result][block][alreadyGeneratedCoins];
 
-$supply  = number_format($supply / 1000000000000, 12, ".", "");
+$supply  = number_format($supply / 10000000, 2, ".", "");
 
 print_r($supply);
 
